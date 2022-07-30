a19e3f1939ac225a67531b64e5d1421320ed0823
marciorasf <marcio.andrade@smarttbotter.com>
2022-07-30 17:44:46 -0300

    Make a test more generic.

--- tests/test_api/test_add_transaction.py
+++ tests/test_api/test_add_transaction.py
@@ -20,12 +20,12 @@ def test_parse_transaction_happy_path() -> None:
 
 
 def test_parse_transaction_missing_field() -> None:
-    t = "client_id=abc-client-1;transaction=2022-07-15T03:40:23.123;value=23.10;description=Chocolate store"
+    t = "clint_id=abc-client-1;transaction=2022-07-15T03:40:23.123;value=23.10;description=Chocolate store"
 
     res = parse_transaction(t)
 
     match res:
         case Err(err):
-            assert err.fields == {"transaction_timestamp"}
+            assert err.fields == {"client_id", "transaction_timestamp"}
         case _:
             pytest.fail("Unreachable")
