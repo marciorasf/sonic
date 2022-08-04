27ec94ae9d9c6b451093c9647a8d7696b64646c8
marciorasf <marcio.andrade@smarttbotter.com>
2022-08-04 19:46:04 -0300

    Merge logging and telemetry modules.

--- sonic/entrypoints/fastapi_app.py
+++ sonic/entrypoints/fastapi_app.py
@@ -8,9 +8,8 @@ from starlette_exporter import PrometheusMiddleware, handle_metrics
 
 from sonic.adapters.repository import FakeRepository
 from sonic.api.router import router
-from sonic.logging import logger, setup_logging
+from sonic.monitoring import logger, setup_logging, setup_telemetry
 from sonic.settings import Settings
-from sonic.telemetry import setup_telemetry
 
 settings = Settings()
 setup_logging()
--- sonic/monitoring.py
+++ sonic/monitoring.py
@@ -1,5 +1,8 @@
+import logging
 from typing import TYPE_CHECKING
 
+import orjson
+import structlog
 from opentelemetry import trace  # type: ignore[attr-defined]
 from opentelemetry.exporter.jaeger.thrift import (  # type: ignore[attr-defined]
     JaegerExporter,
@@ -29,3 +32,21 @@ def setup_telemetry(config: "TelemetryConfig") -> None:
     processor = BatchSpanProcessor(jaeger_exporter)
     provider.add_span_processor(processor)
     trace.set_tracer_provider(provider)
+
+
+def setup_logging() -> None:
+    structlog.configure(
+        cache_logger_on_first_use=True,
+        wrapper_class=structlog.make_filtering_bound_logger(logging.NOTSET),
+        processors=[
+            structlog.contextvars.merge_contextvars,
+            structlog.processors.add_log_level,
+            structlog.processors.format_exc_info,
+            structlog.processors.TimeStamper(fmt="iso", utc=True),
+            structlog.processors.JSONRenderer(serializer=orjson.dumps),
+        ],
+        logger_factory=structlog.BytesLoggerFactory(),
+    )
+
+
+logger = structlog.stdlib.get_logger()
